83f735018b7e36d0768f4e7db8a366ea18b4712e
Brandon Braun <brandonbraun653@protonmail.com>
2024-06-04 06:55:54 -0700

    Figured out the trick for driving full frames

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 
 
 static constexpr uint LED_DATA_PIN    = 0;
-static constexpr uint FREQ_400KHZ     = 800'000;
+static constexpr uint FREQ_800KHZ     = 800'000;
 static constexpr uint PIO_SM          = 0;
 static constexpr uint WS2812_NUM_LEDS = 15;
 static PIO            PIO_INSTANCE    = pio0;
@@ -23,17 +23,16 @@ static uint32_t  raw_led_buffer[ 2 ][ WS2812_NUM_LEDS ];
 static uint32_t *p_render_buffer  = nullptr;
 static uint32_t *p_display_buffer = nullptr;
 
-static critical_section_t s_cs;
-static volatile bool s_swap_request;
-static dma_channel_config dma_cfg;
 
 /**
- * @brief Initialize the PIO to drive the WS2812 LEDs
- *
+ * @brief Initializes the PIO (Programmable I/O) for controlling WS2812 LEDs.
  */
 static void init_pio()
 {
+  // Load the program into the pio peripheral
   uint pio_pgm_offset = pio_add_program( PIO_INSTANCE, &ws2812_program );
+
+  // Initialize the GPIO pin that will be used to drive the WS2812 LEDs
   pio_gpio_init( PIO_INSTANCE, LED_DATA_PIN );
   pio_sm_set_consecutive_pindirs( PIO_INSTANCE, PIO_SM, LED_DATA_PIN, 1, true );
 
@@ -44,7 +43,7 @@ static void init_pio()
 
   // Set the OSR to shift 24 bits out before pulling more data from the TX FIFO.
   // Data format expected by the WS2812 LEDs is GRB with MSB first, so we need
-  // to left shift to match properly.
+  // to left shift to match properly with our system memory order (LSB).
   sm_config_set_out_shift( &cfg, false, true, 24 );
 
   // Set the FIFO join to TX so that the TX FIFO is used
@@ -52,7 +51,7 @@ static void init_pio()
 
   // Configure the PIO clock so that programmed cycle times are correct
   int   cycles_per_bit = ws2812_T1 + ws2812_T2 + ws2812_T3;
-  float div            = clock_get_hz( clk_sys ) / ( FREQ_400KHZ * cycles_per_bit );
+  float div            = clock_get_hz( clk_sys ) / ( FREQ_800KHZ * cycles_per_bit );
   sm_config_set_clkdiv( &cfg, div );
 
   // Initialize and enable the PIO state machine
@@ -63,50 +62,33 @@ static void init_pio()
 
 /**
  * @brief Acknowledge the DMA transfer complete event
- *
  */
 static void dma_complete_callback()
 {
-  dma_hw->ints0 = 1u << dma_channel;
-
-  dma_channel_wait_for_finish_blocking( dma_channel );
-
-  /*---------------------------------------------------------------------------
-  Do the buffer swap
-  ---------------------------------------------------------------------------*/
-  if( s_swap_request )
-  {
-    s_swap_request = false;
-
-    uint32_t *p_temp = p_render_buffer;
-    p_render_buffer  = p_display_buffer;
-    p_display_buffer = p_temp;
-  }
-
-  /*---------------------------------------------------------------------------
-  Set up the parameters for the new transfer
-  ---------------------------------------------------------------------------*/
-  dma_channel_set_trans_count( dma_channel, WS2812_NUM_LEDS, false );
-  dma_channel_set_read_addr( dma_channel, p_display_buffer, true );
-  //dma_channel_start( dma_channel );
+  // This IRQ index selection is coupled with the init_dma() function
+  dma_channel_acknowledge_irq0( dma_channel );
 }
 
 
 /**
- * @brief
+ * @brief Initializes the DMA channel for transferring data to the PIO.
  *
+ * The DMA channel is configured to read from the display buffer and write to
+ * the PIO TX FIFO. The byte ordering is set up so that when combined with the
+ * PIO configuration, data is piped out to the WS2812 LEDs in the correct order.
+ *
+ * Each transfer is a single burst and must be restarted manually to send a new
+ * frame to the LEDs.
  */
 static void init_dma()
 {
-  /*---------------------------------------------------------------------------
-  Claim a DMA channel to use
-  ---------------------------------------------------------------------------*/
-  dma_channel = dma_claim_unused_channel( true );
-  dma_cfg     = dma_channel_get_default_config( dma_channel );
+  dma_channel_config dma_cfg = dma_channel_get_default_config( dma_channel );
 
   /*---------------------------------------------------------------------------
   Configure the DMA channel with the following settings:
   ---------------------------------------------------------------------------*/
+  dma_channel = dma_claim_unused_channel( true );
+
   // Enable byte swapping to convert 32-bit data to MSB first
   channel_config_set_bswap( &dma_cfg, true );
 
@@ -122,7 +104,7 @@ static void init_dma()
   // Set the write increment to not increment the write address
   channel_config_set_write_increment( &dma_cfg, false );
 
-  // Set the transfer size to 0 so that the DMA channel will loop indefinitely
+  // Set the transfer size to a single frame of led data
   dma_channel_set_trans_count( dma_channel, WS2812_NUM_LEDS, false );
 
   // Assign the DMA channel to read from the display buffer
@@ -138,51 +120,49 @@ static void init_dma()
   dma_channel_set_irq0_enabled( dma_channel, true );
   irq_set_exclusive_handler( DMA_IRQ_0, dma_complete_callback );
   irq_set_enabled( DMA_IRQ_0, true );
-
-  /*---------------------------------------------------------------------------
-  Initialize data used for communicating between ISR and buffer swap requests
-  ---------------------------------------------------------------------------*/
-  critical_section_init( &s_cs );
-  s_swap_request = false;
-}
-
-
-/**
- * @brief Performs a blocking swap of the render and display buffers.
- *
- * This will block until the next DMA transfer is complete, upon which the
- * callback ISR will swap the buffers and notify the rendering thread that
- * the new buffer is ready.
- */
-static void buffer_swap()
-{
-  /*---------------------------------------------------------------------------
-  Post a request to the DMA ISR to swap the buffers on the next frame transfer
-  ---------------------------------------------------------------------------*/
-  //critical_section_enter_blocking( &s_cs );
-  s_swap_request = true;
-  //critical_section_exit( &s_cs );
-
-  /*---------------------------------------------------------------------------
-  Just busy poll while waiting. This won't take long.
-  ---------------------------------------------------------------------------*/
-  while( s_swap_request == true )
-  {
-    sleep_ms( 25 );
-  }
 }
 
 
 static void update_leds()
 {
+  /*---------------------------------------------------------------------------
+  Render a new frame of LED data
+  ---------------------------------------------------------------------------*/
   static uint32_t led_idx = 0;
+  static uint32_t color = 0;
 
   memset( p_render_buffer, 0, sizeof( uint32_t ) * WS2812_NUM_LEDS );
 
-  p_render_buffer[ led_idx ] = 0x00FF00;
+  switch( color )
+  {
+    case 0:
+      p_render_buffer[ led_idx ] = 0x110000; // b
+      color++;
+      break;
+    case 1:
+      p_render_buffer[ led_idx ] = 0x001100; // r
+      color++;
+      break;
+    case 2:
+      p_render_buffer[ led_idx ] = 0x00000A; // g
+      color = 0;
+      break;
+  }
+
   led_idx = ( led_idx + 1 ) % WS2812_NUM_LEDS;
 
-  buffer_swap();
+  /*---------------------------------------------------------------------------
+  Wait for the current frame to finish before swapping buffers and kicking off
+  a transfer of the next frame.
+  ---------------------------------------------------------------------------*/
+  dma_channel_wait_for_finish_blocking( dma_channel );
+
+  uint32_t *p_temp = p_render_buffer;
+  p_render_buffer  = p_display_buffer;
+  p_display_buffer = p_temp;
+
+  dma_channel_set_trans_count( dma_channel, WS2812_NUM_LEDS, false );
+  dma_channel_set_read_addr( dma_channel, p_display_buffer, true );
 }
 
 
@@ -191,18 +171,16 @@ int main()
   /*---------------------------------------------------------------------------
   Initialize system resources
   ---------------------------------------------------------------------------*/
-  timer_hw->dbgpause = 0; // Do not pause the timer during debug
+  timer_hw->dbgpause = 0;    // Do not pause the timer during debug
   stdio_init_all();
 
   /*---------------------------------------------------------------------------
   Initialize module memory
   ---------------------------------------------------------------------------*/
-  memset( &raw_led_buffer[ 0 ], 0, sizeof( uint32_t ) * WS2812_NUM_LEDS );
-  memset( &raw_led_buffer[ 1 ], 0, sizeof( uint32_t ) * WS2812_NUM_LEDS );
-
-  p_render_buffer  = raw_led_buffer[ 0 ];
-  p_display_buffer = raw_led_buffer[ 1 ];
-  s_swap_request   = false;
+  p_render_buffer  = &raw_led_buffer[ 0 ][ 0 ];
+  p_display_buffer = &raw_led_buffer[ 1 ][ 0 ];
+  memset( p_render_buffer, 0, sizeof( uint32_t ) * WS2812_NUM_LEDS );
+  memset( p_display_buffer, 0, sizeof( uint32_t ) * WS2812_NUM_LEDS );
 
   /*---------------------------------------------------------------------------
   Configure the default LED pin
@@ -216,17 +194,11 @@ int main()
   init_dma();
   init_pio();
 
-  // Allow some time for the leds to clear
-  sleep_ms( 100 );
-
-  // It seems like this is need to kick off the first transfer? Not sure why.
-  pio_sm_put_blocking( PIO_INSTANCE, PIO_SM, 0 );
-
   bool led_state = false;
   while( 1 )
   {
     update_leds();
     gpio_put( PICO_DEFAULT_LED_PIN, led_state = !led_state );
-    sleep_ms( 500 );
+    sleep_ms( 75 );
   }
 }
