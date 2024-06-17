39412d0edac04c5aa315ae6ba5301ceea64a7db0
Brandon Braun <brandonbraun653@protonmail.com>
2024-06-16 19:57:03 -0700

    Have core0 run the debug client while core1 runs the LED animations

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,16 @@
 #include "animator.hpp"
 #include "buttons.hpp"
 #include "holly_jolly_cfg.hpp"
-#include "pico/stdlib.h"
+#include "pico/multicore.h"
 #include "pico/stdio_usb.h"
+#include "pico/stdlib.h"
+#include "pico_debug.h"
 #include "ws2812.hpp"
 #include <cstring>
 
 
-int main()
+static void core0_entry()
 {
-  /*---------------------------------------------------------------------------
-  Initialize system resources
-  ---------------------------------------------------------------------------*/
-  timer_hw->dbgpause = 0;    // Do not pause the timer during debug
-  stdio_init_all();
-
   /*---------------------------------------------------------------------------
   Initialize hardware resources and the animator subsystem
   ---------------------------------------------------------------------------*/
@@ -27,4 +23,40 @@ int main()
     sleep_ms( FRAME_REFRESH_RATE_MS );
     Animator::process();
   }
+}
+
+
+static void core1_entry()
+{
+  /*---------------------------------------------------------------------------
+  Initialize the USB port
+  ---------------------------------------------------------------------------*/
+  pico_debug_init();
+
+  /*---------------------------------------------------------------------------
+  Start the main loop
+  ---------------------------------------------------------------------------*/
+  pico_debug_core_x_thread();
+}
+
+
+int main()
+{
+  /*---------------------------------------------------------------------------
+  Initialize system resources
+  ---------------------------------------------------------------------------*/
+  timer_hw->dbgpause = 0;    // Do not pause the timer during debug
+  pico_debug_configure_clocks();
+
+  /*---------------------------------------------------------------------------
+  Start the secondary core
+  ---------------------------------------------------------------------------*/
+  multicore_launch_core1( core1_entry );
+
+  /*---------------------------------------------------------------------------
+  Main loop
+  ---------------------------------------------------------------------------*/
+  core0_entry();
+
+  return 0;
 }
